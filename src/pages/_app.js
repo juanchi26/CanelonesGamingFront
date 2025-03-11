6d3ce29fed18c1e874839b613beb51d42cb00ee9
Juan Gabriel Díaz <juanchi.diaz@hotmail.com>
2025-03-11 19:56:19 -0300

    .
    .

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
-import '@/styles/globals.css'
+import 'semantic-ui-css/semantic.min.css'
 
-export default function App({ Component, pageProps }) {
+export default function App(props) {
+  const { Component, pageProps } = props
   return <Component {...pageProps} />
 }
---
+++ src/pages/home/index.js
@@ -0,0 +1,8 @@
+
+export default function HomePage() {
+  return (
+    <div>
+        <h2>Estas en la HOME PAGE</h2>
+    </div>
+  )
+}
