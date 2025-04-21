3018e3af912d4f0d81f8ec31e8938460a0c53235
Juan Gabriel Díaz <juanchi.diaz@hotmail.com>
2025-04-21 17:37:35 -0300

    .
    .

---
+++ src/components/Shared/GridCategories/GridCategories.data.js
@@ -0,0 +1,38 @@
+export  const data = [
+    {
+        id: 1,
+        name: "Notebooks",
+        image: "/img/categories-home/portatiles.jpg",
+        link: "/categories/notebooks",
+    },
+    {
+        id: 2,
+        name: "Torres Gaming",
+        image: "/img/categories-home/pc-sobremesa.jpg",
+        link: "/categories/Torres-Gaming",
+    },
+    {
+        id: 3,
+        name: "Monitores",
+        image: "/img/categories-home/monitores.jpg",
+        link: "/categories/monitores",
+    },
+    {
+        id: 4,
+        name: "Ratones",
+        image: "/img/categories-home/ratones.jpg",
+        link: "/categories/ratones",
+    },
+    {
+        id: 5,
+        name: "Celulares",
+        image: "/img/categories-home/telefonos.jpg",
+        link: "/categories/celulares",
+    },
+    {
+        id: 6,
+        name: "Teclados",
+        image: "/img/categories-home/teclados.jpg",
+        link: "/categories/teclados",
+    },
+]
--- src/components/Shared/GridCategories/GridCategories.js
+++ src/components/Shared/GridCategories/GridCategories.js
@@ -1,8 +1,18 @@
 import styles from './GridCategories.module.scss'
+import { Image } from "semantic-ui-react"
+import Link, { link } from "next/link"
+import { map } from "lodash"
+import { data } from "./GridCategories.data"
 export function GridCategories() {
   return (
-    <div>
-      <h2>GridCategories</h2>
+    <div className={styles.container}>
+      {map(data, (category) => (
+        <Link key={category.id} href={category.link}>
+            <div className={styles.category}>
+                <Image src={category.image} alt={category.name} />
+            </div>
+        </Link>
+      ))}
     </div>
   )
 }
